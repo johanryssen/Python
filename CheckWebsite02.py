245a1a7e0adcdcedb6a1af66eb4843a82540270b
jvryssen <johanvanryssen@gmail.com>
2019-04-30 07:15:53 +1000

    Check Website x 4

---
+++ CheckWebsite02.py
@@ -0,0 +1,24 @@
+import socket
+import urllib.request
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip = s.getsockname()[0]
+    s.close()
+    return ip
+
+# Check status of website
+print('TASK [Check status of website]' + ' ' + (20 * '*'))
+address = 'http://' + get_ip()
+try:
+    response = requests.get(url)
+	if response.status_code == 200:
+        print('Website is up.')
+        print('True')
+    else:
+        print('Website appears to be down.')
+        print('False')
+except:
+    print('Website appears to be down.')
+    print('False')
